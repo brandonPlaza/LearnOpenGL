1135a982cbad727baf0af4620a19fd19f3a1914c
brandonPlaza <plazab@sheridancollege.ca>
2022-06-10 21:51:55 -0400

    Created base for the LearnOpenGL course

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kpanik/Documents/Personal/Projects/LearnOpenGL
+# Build directory: /home/kpanik/Documents/Personal/Projects/LearnOpenGL/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
